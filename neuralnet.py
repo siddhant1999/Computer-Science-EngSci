84610667dad3b610aea0b6a60859090b37254911
Siddhant Jain <siddhant99.jain@hotmail.com>
2018-02-23 03:25:52 -0500

    Some minor additions

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class NeuralNet():
 	inp = []
@@ -10,23 +11,37 @@ class NeuralNet():
 		self.n= [[0 for j in range(3)] for i in range(4)]
 		self.n[0][0]=1
 		print self.w
-		print "*"
 		print self.n
+
+	def sigmoid(self, x):
+		return 1 / (1 + math.exp(-x))
+
+	def der(self, x):
+		return self.sigmoid(x)*(1-self.sigmond(x))
+
 
 
 	def forward(self):
 		neurons=[2,3,3,1]
+		bias = np.random.uniform(low=-1, high=1, size=(3))
+		# the size of the bias array is one less than the number of layers
 		print "*"
+
 		for i in range(1,4):
 			for j in range(neurons[i]):
-				s= 0
+				s= bias[i-1]
 				for k in range(neurons[i-1]):
 					s+= self.n[i-1][k] * self.w[i-1][k][j]
-				self.n[i][j] = np.tanh(s)
+				self.n[i][j] = self.sigmoid(s)
 		print "n: ", self.n
 		
 	def backwards(self, ans):
-		err = np.absolute(ans-self.n[3][0])
+		err = ans-self.n[3][0]
+		errsquared = err*err/2.0 #this is simply the error and isn't used for calculations
+		de = -1*(ans - )
+		#let's write a method for finding the amount by which each weight should be changed for a single layer
+
+
 
 
 
