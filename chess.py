58925b7050268a02e783529f620a97cca803266a
Siddhant Jain <siddhant99.jain@hotmail.com>
2018-02-23 19:28:32 -0500

    Basic gameplay functionality implemented

--- chess.py
+++ chess.py
@@ -2,7 +2,51 @@
    where board is the list data struct that represents the chess board
    (described above) and player is 10 (for white) and 20 (for black).
    It should return a list of all positions that the player occupies.'''
+def printBoard(board):
+	for i in range(len(board)):
+		if i%8==0:
+			print "\n"
+		if board[i] == 0:
+			if i%2:
+				print "#",
+			else:
+				print ".",
+		elif board[i]%10 == 0:
+			if isWhite(board[i]):
+				print "P",
+			else:
+				print "p",
 
+		elif board[i]%10 == 1:
+			if isWhite(board[i]):
+				print "H",
+			else:
+				print "h",
+
+		elif board[i]%10 == 2:
+			if isWhite(board[i]):
+				print "B",
+			else:
+				print "b",
+
+		elif board[i]%10 == 3:
+			if isWhite(board[i]):
+				print "R",
+			else:
+				print "r",
+
+		elif board[i]%10 == 4:
+			if isWhite(board[i]):
+				print "Q",
+			else:
+				print "q",
+
+		elif board[i]%10 == 5:
+			if isWhite(board[i]):
+				print "K",
+			else:
+				print "k",
+	print "\n"
 
 def GetPlayerPositions(board,player):
 	l =[]
@@ -15,6 +59,18 @@ def GetPlayerPositions(board,player):
 '''Write the function GetPieceLegalMoves(board,position)
    which will return a list of all legal positions that the piece in
    the given position can take.'''
+'''
+Write the function IsPositionUnderThreat(board,position,player)
+   which will return True if the position is under threat by the opponent
+   of the given player
+   '''
+def IsPositionUnderThreat(board,position,player):
+	for i in range(len(board)):
+		if isWhite(board[i]) != isWhite(position):
+			o = GetPieceLegalMoves(board, i)
+			if position in o:
+				return True
+	return False
 
 def inCheck(board, colour):
 	#let's us first check if white king is in danger
@@ -56,32 +112,32 @@ def GetPieceLegalMoves(board,position):
 	if p == 0:
 		#pawn
 
-		if isWhite:
+		if isWhite(board[position]):
 			if position>=56:
-				return
+				return l
 			if board[position+8]==0:
 				l.append(position+8)
 
 			#capture
 			if position+7 < 64:
-				if board[position+7]>=20:
+				if isWhite(board[position+7]) != isWhite(board[position]):
 					l.append(position+7)
 
 			if position+9 < 64:
-				if board[position+9]>=20:
+				if isWhite(board[position+9]) != isWhite(board[position]):
 					l.append(position+9)	
 		else:
 			if position<=7:
-				return
+				return l
 			if board[position-8]==0:
 				l.append(position-8)
 			#capture
 			if position-7 >= 0:
-				if board[position-7]>=10 and board[position-7] < 20:
+				if isWhite(board[position-7]) != isWhite(board[position]):
 					l.append(position-7)
 
 			if position-9 >= 0:
-				if board[position-9]>=10 and board[position-9] < 20:
+				if isWhite(board[position-9]) != isWhite(board[position]):
 					l.append(position-9)
 
 		return l
@@ -270,7 +326,33 @@ def GetPieceLegalMoves(board,position):
 					l.append(pos)
 		return l
 
-			
+def letsPlay(board, player):
+	printBoard(board)
+	t=raw_input("Enter the coordinates:\n")
+	s = raw_input()
+	t=int(t)
+	s=int(s)
+	#print 8*t+s
+	print board[8*t+s], player
+	#print isWhite(board[8*t+s])
+	#print isWhite(player)
+
+	if isWhite(board[8*t+s]) != isWhite(player):
+		letsPlay(board, player)
+
+	a = raw_input("Enter the final coordinates:\n")
+	b = raw_input()
+	a=int(a)
+	b=int(b)
+	print GetPieceLegalMoves(board, 8*t+s)
+	q = a*8 + b
+	if q in GetPieceLegalMoves(board, 8*t+s):
+		board[q] = board[8*t+s]
+		board[8*t+s] = 0
+		if player == 10:
+			letsPlay(board, 20)
+		if player == 20:
+			letsPlay(board, 10)
 
 s = [
 	13, 11, 12, 14, 15, 12, 11, 13,
@@ -282,5 +364,7 @@ s = [
 	20, 20, 20, 20, 20, 20, 20, 20,
 	23, 21, 22, 24, 25, 22, 21, 23,
 	]
-print GetPieceLegalMoves(s,4)
+#print GetPieceLegalMoves(s,4)
+#printBoard(s)
 
+letsPlay(s, 10)
