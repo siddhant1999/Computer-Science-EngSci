8a44a4f4f2b25e43673d81d7e7dd0c545930b15b
Siddhant Jain <siddhant99.jain@hotmail.com>
2018-03-06 13:15:27 -0500

    Lab complete

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 class graph:
-	edges =[[None] * 5 for i in range(5)]
+	edges =[[None] * 1000 for i in range(1000)]
 	line=[]
 	visited =[]
 	tot=[]
@@ -7,21 +7,26 @@ class graph:
 		self.vertices=0
 
 	def addVertex(self, n):
+		if n < 0:
+			return -1
 		self.vertices+= n
 		return self.vertices
 
 	def addEdge(self, from_idx, to_idx, directed, weight):
+		if weight < 0 or from_idx < 0 or to_idx < 0:
+			return False
 		#print from_idx, to_idx, weight
 		self.edges[from_idx][to_idx] = weight
 		if not directed:
 			self.edges[to_idx][from_idx] = weight
 		#print self.edges
+		return True
 
 	def BFS(self, start, visited):
 		t= []
 		line = [start]
 		for i in range(100000):
-			if i==len(self.line):
+			if i==len(line):
 				break
 			if line[i] in visited:
 				continue
@@ -35,10 +40,14 @@ class graph:
 
 
 	def traverse(self, start, typeBreadth):
+		if start != None and start >= self.vertices:
+			return []
 		#print self.edges
 		#still need to make sure start can be None
 		#also make sure you check indices that are not connected
+		s=0
 		if start == None:
+			s = None
 			start = 0
 
 		self.line = [start]
@@ -64,14 +73,21 @@ class graph:
 			self.tot=[]
 			self.dfs(start)
 			
-		if start == None:
-			for i in range(len(self.vertices)):
-				if i not in self.tot:
+		if s == None:
+			newtot = [self.tot]
+			
+			for i in range(self.vertices):
+				self.tot= []
+				if not any(i in sublist for sublist in newtot):
 					if typeBreadth:
-						self.tot+=BFS(i, visited)
+						newtot+=[self.BFS(i, visited)]
 					else:
 						self.dfs(i)
-		print self.tot
+						newtot.append( self.tot)
+			return newtot
+
+
+		return self.tot
 
 	def dfs(self, index):
 		if index in self.visited:
@@ -120,7 +136,7 @@ class graph:
 
 
 		
-
+'''
 x = graph()
 
 x.addVertex(4)
@@ -129,3 +145,28 @@ x.addEdge(0, 3, True, 1)
 x.addEdge(3, 2, True, 1)
 x.addEdge(3, 1, True, 1)
 x.traverse(None, False)
+'''
+
+x=graph()
+x.addVertex(1)
+x.addVertex(1)
+x.addVertex(1)
+x.addVertex(1)
+x.addVertex(1)
+#x.printEdges()
+x.addEdge(0,1,False,1)
+#x.addEdge(0,2,False,1)
+x.addEdge(0,3,False,1)
+#x.addEdge(1,2,False,2)
+x.addEdge(1,4,False,2)
+x.addEdge(2,3,True,3)
+x.addEdge(3,4,True,4)
+x.addEdge(4,5,True,5)
+#x.printEdges()
+print "Depth"
+print x.traverse(0,False)
+
+print "Breadth"
+print x.traverse(0,True)
+print "Breadth; start=None"
+print x.traverse(None,False)
