426ef9c1625432d7da3cc07cd59879f41f68132a
Siddhant Jain <siddhant99.jain@hotmail.com>
2018-03-26 23:09:49 -0400

    Sorting

---
+++ sortlib.py
@@ -0,0 +1,61 @@
+
+def selection_sort(u):
+   for i in range(len(u)):
+    min_idx = i
+    for j in range(i+1, len(u)):
+        if u[min_idx] > u[j]:
+            min_idx = j
+             
+    # Swap the found minimum element with 
+    # the first element        
+    u[i], u[min_idx] = u[min_idx], u[i]
+   return True
+
+def heapify(u):
+   
+   return True
+
+def reheapify(u,end):
+      return True
+
+def heap_sort(u):
+   
+   heapify(u)
+   #reheapify(u,end)
+   return True
+
+def merge_sort(u):
+   return True
+
+def quick_sort(u,ini,fin):
+   #pIndex = partition(u,ini,fin)
+   return True
+
+def partition(u,ini,fin):
+   return pIndex
+
+
+v1=[10,9,8,7,6,5,4,3,2,1,0]
+v2=[100,1,1000,9,8,7,2,2000,10]
+v3=[100,10,1000,9,8,7,2,6,5,2,3,1]
+
+for i in [ v1,v2,v3 ]:
+   x=list(i)
+   selection_sort(x)
+   print x
+
+   x=list(i)
+   heapify(x)
+   print x
+
+   x=list(i)
+   heap_sort(x)
+   print x
+
+   x=list(i)
+   merge_sort(x)
+   print x
+
+   x=list(i)
+   quick_sort(x,0,len(x)-1)
+   print x
